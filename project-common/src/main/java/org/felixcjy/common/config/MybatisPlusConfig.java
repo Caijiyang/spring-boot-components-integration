154d5be9bcb95d6e8a0009c267d5fd6df419058c
FelixCJY <3049606735@qq.com>
2025-05-25 12:08:09 +0800

    feat: MybatisPlusConfig 配置更新
    依据数据库自动选择分页器

--- project-common/src/main/java/org/felixcjy/common/config/MybatisPlusConfig.java
+++ project-common/src/main/java/org/felixcjy/common/config/MybatisPlusConfig.java
@@ -4,22 +4,56 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Mybatis Plus 配置类
+ * 动态配置分页器，请务必配置 mybatis-plus.db-type 的值。
+ *
  * @author: Felix(蔡济阳)
  * @since : 2025/2/23 17:54
  */
 @Configuration
 @MapperScan({"org.felixcjy.**.mapper*"})
 public class MybatisPlusConfig {
-    /** 添加分页插件 */
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${mybatis-plus.db-type}")
+    private String dbType;
+
+    /**
+     * 注册 MyBatis-Plus 插件拦截器
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(resolveDbType()));
         return interceptor;
+    }
+
+    /** 动态识别数据库类型 */
+    private DbType resolveDbType() {
+        try (Connection conn = dataSource.getConnection()) {
+            String url = conn.getMetaData().getURL().toLowerCase();
+            if (url.contains("mysql")) {
+                return DbType.MYSQL;
+            } else if (url.contains("oracle")) {
+                return DbType.ORACLE;
+            } else if (DbType.getDbType(dbType) != null) {
+                return DbType.getDbType(dbType);
+            } else {
+                throw new UnsupportedOperationException("不支持的数据库类型: " + url);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("无法获取数据库连接，无法识别数据库类型", e);
+        }
     }
 }
